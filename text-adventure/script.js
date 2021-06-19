73d955865c56570f31bc70d664aa507d0d76cd9b
Conlan Cason <conlan.cason9@gmail.com>
2021-06-19 15:29:25 -0600

    Set up user input and local storage for the text adventure

--- text-adventure/script.js
+++ text-adventure/script.js
@@ -1,49 +1,19 @@
-const myStorage = window.localStorage;
 
-localStorage.setItem('myCat', 'Tom');
-const cat = localStorage.getItem('myCat');
-localStorage.removeItem('myCat');
 
-localStorage.clear(); //clear if player restarts the game
-
-function storageAvailable(type) {
-    var storage;
-    try {
-        storage = window[type];
-        var x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return true;
-    }
-    catch(e) {
-        return e instanceof DOMException && (
-            // everything except Firefox
-            e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === 'QuotaExceededError' ||
-            // Firefox
-            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-            // acknowledge QuotaExceededError only if there's something already stored
-            (storage && storage.length !== 0);
-    }
+if (localStorage.getItem('name')) {
+	setAdventurersname();
 }
 
-if (!storageAvailable('localStorage')) {
-	alert('localStorage not available; game will not work properly. '
-		+ 'You might be in a private or incognito browsing mode, or your browser does not support localStorage.');
+function setAdventurersname() {
+	document.getElementById('nameform').value = localStorage.getItem('name');
+	document.getElementById('adventurersname').innerHTML = localStorage.getItem('name');
 }
 
-document.getElementById('adventurersname').innerHTML = 'still not yet';
+function populateStorage() {
+	localStorage.setItem('name', document.getElementById('nameform').value);
+	setAdventurersname();
+}
 
-if (!localStorage.getItem('name')) {
-	localStorage.setItem('name', document.getElementById('name').value);
-	alert('if');
-}
-else {
-	name = localStorage.getItem('name');
-	document.getElementById('adventurersname').innerHTML = name;
-	alert('else');
-}
+document.getElementById('nameform').onchange = populateStorage;
+
+document.getElementsByClassName('name').innerHTML = localStorage.getItem('name');
---
+++ text-adventure/storage-available-script.js
@@ -0,0 +1,29 @@
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+if (!storageAvailable('localStorage')) {
+	alert('localStorage not available; game will not work properly. '
+		+ 'You might be in a private or incognito browsing mode, or your browser does not support localStorage.');
+}
